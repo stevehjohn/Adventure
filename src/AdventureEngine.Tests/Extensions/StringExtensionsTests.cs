e266321200dff9aa82320c942c55922305c9870a
Stevö John <stevehuwjohn@gmail.com>
2019-05-12 12:10:27 +0100

    WIP

---
+++ src/AdventureEngine.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using AdventureEngine.Extensions;
+using NUnit.Framework;
+
+namespace AdventureEngine.Tests.Extensions
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void RemovePunctuation_removes_punctuation()
+        {
+            Assert.That("This, string. has: varying' punctuation% characters".RemovePunctuation(), Is.EqualTo("This string has varying punctuation characters"));
+        }
+    }
+}
