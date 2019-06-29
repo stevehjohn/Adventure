f69486737d2f22fcca5700c7d139a65668e176b3
Stevö John <stevehuwjohn@gmail.com>
2019-06-29 11:43:41 +0100

    Tidying up

--- src/AdventureEngine/Models/Item.cs
+++ src/AdventureEngine/Models/Item.cs
@@ -1,7 +1,11 @@
-﻿namespace AdventureEngine.Models
+﻿using System;
+
+namespace AdventureEngine.Models
 {
     public class Item
     {
         public string Description { get; set; }
+
+        public Func<string, Game> Use { get; set; }
     }
 }
